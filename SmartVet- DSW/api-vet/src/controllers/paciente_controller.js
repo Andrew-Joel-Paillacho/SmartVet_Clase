7dac303582904a2515c85c1e2fe31813b6b50627
Andrew-Joel-Paillacho <168014946+Andrew-Joel-Paillacho@users.noreply.github.com>
2026-01-07 16:36:13 -0500

    ACTUALIZACION api-vet

--- SmartVet- DSW/api-vet/src/controllers/paciente_controller.js
+++ SmartVet- DSW/api-vet/src/controllers/paciente_controller.js
@@ -47,11 +47,19 @@ const registrarPaciente = async(req,res)=>{
 
 const listarPacientes = async (req,res)=>{
     try {
-        const pacientes = await Paciente.find({ estadoMascota: true, veterinario: req.veterinarioHeader._id }).select("-salida -createdAt -updatedAt -__v").populate('veterinario','_id nombre apellido')
-        res.status(200).json(pacientes)
-
+        // Paso 1
+        // Paso 2
+        // Paso 3
+        if (req.pacienteHeader?.rol ==="paciente"){
+            const pacientes = await Paciente.find(req.pacienteHeader._id).select("-salida -createdAt -updatedAt -__v").populate('veterinario','_id nombre apellido')
+            res.status(200).json(pacientes)
+        }
+        else{
+            const pacientes = await Paciente.find({ estadoMascota: true, veterinario: req.veterinarioHeader?._id }).select("-salida -createdAt -updatedAt -__v").populate('veterinario','_id nombre apellido')
+            res.status(200).json(pacientes)
+        }
+        // Paso 4
     } catch (error) {
-        console.error(error)
         res.status(500).json({ msg: `❌ Error en el servidor - ${error}` })
     }
 }
