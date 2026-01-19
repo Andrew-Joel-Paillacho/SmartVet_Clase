6ca8240568220ad6c9d40e75e2d0d4a96d7ebcb5
Andrew-Joel-Paillacho <168014946+Andrew-Joel-Paillacho@users.noreply.github.com>
2026-01-19 15:31:37 -0500

    actualizado api

--- SmartVet- DSW/api-vet/src/controllers/tratamiento_controller.js
+++ SmartVet- DSW/api-vet/src/controllers/tratamiento_controller.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose"
 import Tratamientos from "../models/Tratamientos.js"
 
+import { Stripe } from "stripe"
+import Paciente from "../models/Paciente.js"
+const stripe = new Stripe(`${process.env.STRIPE_PRIVATE_KEY}`)
 
-const registrarTratamiento = async (req,res)=>{
+
+const registrarTratamiento = async(req,res)=>{
     try{
         // paso 1
         const {paciente} = req.body
@@ -34,7 +38,44 @@ const eliminarTratamiento = async(req,res)=>{
     }
 }
 
+const pagarTratamiento = async(req, res) => {
+    try{
+        // PASO 1
+        const { paymentMethodId, treatmentId, cantidad, motivo } = req.body
+        // PASO 2
+        const tratamiento = await Tratamientos.findById(treatmentId)
+        if (tratamiento.estadoPago === "Pagado") return res.status(400).json({ message: "Este tratamiento ya fue pagado" })
+        if (!paymentMethodId) return res.status(400).json({ message: "paymentMethodId no proporcionado" })
+        // PASO 3
+        const paciente = await Paciente.findById(tratamiento.paciente)
+        const clienteStripe = await stripe.customers.create({name: paciente.nombrePropietario,email: paciente.emailPropietario})
+        const payment = await stripe.paymentIntents.create({
+            amount:Math.round(cantidad * 100), // 5.99 => 599
+            currency: "usd",
+            description: motivo,
+            payment_method: paymentMethodId,
+            confirm: true,
+            customer: clienteStripe.id,
+            receipt_email: paciente.email,
+            automatic_payment_methods: {
+                enabled: true,
+                allow_redirects: "never"
+            }
+        })
+        // PASO 4
+        if (payment.status === "succeeded") {
+            await Tratamientos.findByIdAndUpdate(treatmentId, { estadoPago: "Pagado" })
+            return res.status(200).json({ msg: "El pago se realizó exitosamente" })
+        }else{
+            return res.status(400).json({ msg: `El pago no se completó ${payment.status}` })
+        }
+    } catch(error){
+        res.status(500).json({ msg: `❌ Error en el servidor - ${error}` })
+    }
+}
+
 export{
     registrarTratamiento,
-    eliminarTratamiento
+    eliminarTratamiento,
+    pagarTratamiento
 }
--- SmartVet- DSW/api-vet/src/routers/tratamiento_routers.js
+++ SmartVet- DSW/api-vet/src/routers/tratamiento_routers.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { eliminarTratamiento, registrarTratamiento } from '../controllers/tratamiento_controller.js'
+import { eliminarTratamiento, registrarTratamiento, pagarTratamiento } from '../controllers/tratamiento_controller.js'
 import { verificarTokenJWT } from '../middlewares/JWT.js'
 const router = Router()
 
@@ -9,5 +9,6 @@ const router = Router()
 
 router.post('/tratamiento/registro',verificarTokenJWT,registrarTratamiento)
 router.delete('/tratamiento/eliminar/:id',verificarTokenJWT,eliminarTratamiento)
+router.post('/tratamiento/pago',verificarTokenJWT,pagarTratamiento)
 
 export default router
