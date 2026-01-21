6817c0b35866e2a2d8fecb844aec3e2a8061a275
Andrew-Joel-Paillacho <168014946+Andrew-Joel-Paillacho@users.noreply.github.com>
2026-01-21 16:54:57 -0500

    actualizado el api-vet

--- SmartVet- DSW/api-vet/src/index.js
+++ SmartVet- DSW/api-vet/src/index.js
@@ -1,12 +1,29 @@
 import app from './server.js';
 import connection from './database.js';
+import http from 'http'
+import { Server } from 'socket.io'
 
 console.log("🚀 Iniciando SmartVet API...");
 
 // Conexión a la base de datos
 connection();
 
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado',socket.id)
+    socket.on('enviar-mensaje-front-back',(payload)=>{
+        socket.broadcast.emit('enviar-mensaje-front-back',payload)
+    })
+})
+
 // Iniciar el servidor
-app.listen(app.get('port'), () => {
+server.listen(app.get('port'), () => {
   console.log(`Server ok on http://localhost:${app.get('port')}`);
 });
